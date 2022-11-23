43aca11e7bf1971797050d4fc5d3f75fb63a326d
abisheik88 <abishekvijayarangan@gmail.com>
2022-11-23 15:25:30 +0530

    ctrl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='container'></div>
+        <div className='row'></div>
+
+      <Card />
     </div>
   );
+}
+function Card(){
+  return(
+  <div className='col-lg-4'>
+    <div className="card border-success mb-3" >
+  <div className="card-header bg-transparent border-success">Header</div>
+  <div className="card-body text-success">
+    <h5 className="card-title">Success card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div className="card-footer bg-transparent border-success">Footer</div>
+</div>
+  </div>
+
+  )
+
 }
 
 export default App;
